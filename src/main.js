2b94f6dabf5ac9bb7328aeab940e7d6966e3b5d5
D-Queck <76432409+D-Queck@users.noreply.github.com>
2025-04-08 14:53:24 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import './hardware-sensor-platform.js';
-import './header-blur.js'; // falls du den Code modular halten willst
+import './header-blur.js';
 
 
