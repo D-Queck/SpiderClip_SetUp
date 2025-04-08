bebe83a15f50a2d03a69b56885522748d58132e1
D-Queck <76432409+D-Queck@users.noreply.github.com>
2025-04-08 11:25:02 +0200

    Init changes

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+import './hardware-sensor-platform.js';
+import './header-blur.js'; // falls du den Code modular halten willst
+
+
