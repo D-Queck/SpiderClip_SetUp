03553637adaf32972dc81c5fab1fbe12befaf04d
D-Queck <76432409+D-Queck@users.noreply.github.com>
2025-04-18 15:16:52 +0200

    delete vite svg

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import './hardware-sensor-platform.js';
 import './vr-wearable.js';
 import './header-blur.js';
