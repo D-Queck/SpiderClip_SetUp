48795c7dd267a9dae9ba88d03edcdc5f73169f61
D-Queck <76432409+D-Queck@users.noreply.github.com>
2025-06-09 07:22:00 +0200

    fix: use viteStaticCopy for components and use BASE_URL

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/SpiderClip_SetUp/',           // bleibt so
+  base: '/SpiderClip_SetUp/',   // wichtig für GitHub Pages
   plugins: [
     viteStaticCopy({
       targets: [
-        // kopiert src/components 1-zu-1 nach dist/src/components
-        { src: 'src/components', dest: 'src' }
+        { src: 'src/components', dest: 'src' },   // Kopiere src/components in dist/src
+        { src: 'src/code', dest: 'src' }          // Kopiere src/code in dist/src
       ]
     })
   ]
