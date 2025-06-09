1c5022d2736d259b2d908325668fc087f73a002e
D-Queck <76432409+D-Queck@users.noreply.github.com>
2025-06-09 12:02:15 +0200

    update central.ino algorithm

--- src/code/central.ino
+++ src/code/central.ino
@@ -1,128 +1,98 @@
 #include <ArduinoBLE.h>
-    
-//for step counting
-float vector=0; 
-float oldvector =0 ;
-int step =0;
+#include "DFRobot_Heartrate.h"
+#include <Arduino_LSM6DS3.h>
 
-// --------------------- Setup ------------------------
+#define heartratePin A0
+int  PowerPin = 12;
+DFRobot_Heartrate heartrate(DIGITAL_MODE);   // ANALOG_MODE oder DIGITAL_MODE
+
+BLEService        sensorService("19B10000-E8F2-537E-4F6C-D104768A1214");
+BLEIntCharacteristic ConfidenceCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1212", BLERead);
+BLEIntCharacteristic OxygenCharacteristic    ("19B10001-E8F2-537E-4F6C-D104768A1213", BLERead);
+BLEIntCharacteristic HrCharacteristic        ("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead);
+BLEIntCharacteristic AccxCharacteristic      ("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead);
+BLEIntCharacteristic AccyCharacteristic      ("19B10001-E8F2-537E-4F6C-D104768A1216", BLERead);
+BLEIntCharacteristic AcczCharacteristic      ("19B10001-E8F2-537E-4F6C-D104768A1217", BLERead);
+
 void setup() {
-Serial.begin(9600);
-while (!Serial);
-Serial.println("started");
-BLE.begin(); // initialize the BLE hardware
-Serial.println("BLE Central - LED control");
-BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");// start scanning for peripherals
+  pinMode(PowerPin, OUTPUT);
+  digitalWrite(PowerPin, HIGH);
+
+  Serial.begin(9600);
+  delay(2000);
+  Serial.println("Sensor-Plattform gestartet");
+
+  if (!IMU.begin()) {
+    Serial.println("IMU-Initialisierung fehlgeschlagen!");
+    while (1);
+  }
+  if (!BLE.begin()) {
+    Serial.println("BLE-Initialisierung fehlgeschlagen!");
+    while (1);
+  }
+
+  BLE.setLocalName("SpiderClip_Sensor");
+  BLE.setAdvertisedService(sensorService);
+
+  sensorService.addCharacteristic(HrCharacteristic);
+  sensorService.addCharacteristic(ConfidenceCharacteristic);
+  sensorService.addCharacteristic(OxygenCharacteristic);
+  sensorService.addCharacteristic(AccxCharacteristic);
+  sensorService.addCharacteristic(AccyCharacteristic);
+  sensorService.addCharacteristic(AcczCharacteristic);
+
+  BLE.addService(sensorService);
+  HrCharacteristic.writeValue(0);
+  ConfidenceCharacteristic.writeValue(0);
+  OxygenCharacteristic.writeValue(0);
+  AccxCharacteristic.writeValue(0);
+  AccyCharacteristic.writeValue(0);
+  AcczCharacteristic.writeValue(0);
+
+  BLE.advertise();
+  Serial.println("BLE Sensor Peripheral aktiv");
 }
 
-// --------------------- Loop ------------------------
 void loop() {
-BLEDevice peripheral = BLE.available(); // check if a peripheral has been discovered
-if (peripheral) { // discovered a peripheral, print out address, local name, and advertised service
-Serial.print("Found ");
-Serial.print(peripheral.address());
-Serial.print(" '");
-Serial.print(peripheral.localName());
-Serial.print("' ");
-Serial.print(peripheral.advertisedServiceUuid());
-Serial.println();
-if (peripheral.localName() != "LED") {
-    return;
-}
-BLE.stopScan(); // stop scanning
-controlLed(peripheral);
-BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214"); // peripheral disconnected, start scanning again
-}
-}
+  BLEDevice central = BLE.central();
+  if (!central) return;
 
-// --------------------- BLE Function ------------------------
-void controlLed(BLEDevice peripheral) {
-Serial.println("Connecting ..."); // connect to the peripheral
+  Serial.print("Verbunden mit: ");
+  Serial.println(central.address());
 
-if (peripheral.connect()) {
-Serial.println("Connected");
-} else {
-Serial.println("Failed to connect!");
-return;
-}
-// discover peripheral attributes
-// Serial.println("Discovering attributes ...");
-if (peripheral.discoverAttributes()) {
-//Serial.println("Attributes discovered");
-} else {
-// Serial.println("Attribute discovery failed!");
-peripheral.disconnect();
-return;
-}
-// retrieve the LED characteristic
-BLECharacteristic ConfidenceCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1212");
-BLECharacteristic OxygenCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1213");
-BLECharacteristic HrCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1214");
-BLECharacteristic AccxCharacteristic= peripheral.characteristic("19B10001-E8F2-537E-4F6C-D104768A1215");
-BLECharacteristic AccyCharacteristic= peripheral.characteristic("19B10001-E8F2-537E-4F6C-D104768A1216");
-BLECharacteristic AcczCharacteristic= peripheral.characteristic("19B10001-E8F2-537E-4F6C-D104768A1217");
+  while (central.connected()) {
+    // Herzfrequenz
+    heartrate.getValue(heartratePin);
+    uint8_t rate = heartrate.getRate();
+    Serial.print("HR: "); Serial.println(rate);
+    HrCharacteristic.writeValue(rate);
 
-if (!HrCharacteristic) {
-// Serial.println("Peripheral does not have LED characteristic!");
-peripheral.disconnect();
-return;
-} else if (!HrCharacteristic.canWrite()) {
-//Serial.println("Peripheral does not have a writable LED characteristic!");
-peripheral.disconnect();
-return;
-}
+    // Confidence & Oxygen aus body-Struct der Bibliothek
+    Serial.print("Conf: "); Serial.print(body.confidence);
+    Serial.print("  O2: ");   Serial.println(body.oxygen);
+    ConfidenceCharacteristic.writeValue(body.confidence);
+    OxygenCharacteristic.writeValue(body.oxygen);
 
-while (peripheral.connected()) {// while the peripheral is connected
-float x,y,z =0;
-int Hr=0;
-int Confidence=0;
-int Oxygen=0;
-//READ THE CHARACTERISTICS HERE
-HrCharacteristic.read();
-ConfidenceCharacteristic.read();
-OxygenCharacteristic.read();
-AccxCharacteristic.read();
-AccyCharacteristic.read();
-AcczCharacteristic.read();
-Hr = returnInt(HrCharacteristic.value()); //TRANSFORM BYTES TO INT
-x=0.001*(float) returnInt(AccxCharacteristic.value());
-y=0.001*(float) returnInt(AccyCharacteristic.value());
-z=0.001*(float) returnInt(AcczCharacteristic.value());
-vector = sqrt( x*x + y*y + z*z);
-if (abs(vector - oldvector)>0.25)
-step++;
-oldvector = vector;
-Serial.print(step);
-//SENDING VALUE TO JSON AREA//*******************************
-Serial.print("{");
-Serial.print("\"b\":");
-Serial.print(Hr);
-Serial.print(",");
-Serial.print("\"step\":");
-Serial.print(step);
-Serial.print(",");
-Serial.print("\"ax\":");
-Serial.print(x);
-Serial.print(",");
-Serial.print("\"ay\":");
-Serial.print(y);
-Serial.print(",");
-Serial.print("\"az\":");
-Serial.print(z);
-Serial.println("}");
-delay(100);
-//***********************************************************
-}
-Serial.println("Peripheral disconnected");
-}
+    // Beschleunigung
+    float x, y, z;
+    if (IMU.accelerationAvailable()) {
+      IMU.readAcceleration(x, y, z);
+      int ix = (int)(x * 1000);
+      int iy = (int)(y * 1000);
+      int iz = (int)(z * 1000);
+      Serial.print("Acc (mg): ");
+      Serial.print(ix); Serial.print(", ");
+      Serial.print(iy); Serial.print(", ");
+      Serial.println(iz);
 
-// --------------------- Return Int ------------------------
-int returnInt(const unsigned char data[])
-{
-int a=
-(int)data[0]        |
-(int)data[1]  << 8  |
-(int)data[2]  << 16 |
-(int)data[3]  << 24 ; 
-return a;
+      AccxCharacteristic.writeValue(ix);
+      AccyCharacteristic.writeValue(iy);
+      AcczCharacteristic.writeValue(iz);
+    }
+
+    delay(100);
+  }
+
+  Serial.print("Verbindung getrennt: ");
+  Serial.println(central.address());
 }
